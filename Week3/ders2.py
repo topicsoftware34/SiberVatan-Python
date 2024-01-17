e905be8fc31f1094e0be8f84797b9f0881663129
topicsoftware34 <155827627+topicsoftware34@users.noreply.github.com>
2024-01-17 19:10:00 +0300

    Create ders2.py
    type , str, int dönüşüm 
    input

---
+++ Week3/ders2.py
@@ -0,0 +1,42 @@
+# sayi1 = 42
+# sayi2 = -17
+# print(type(sayi2))
+
+# sayi3 = 1.5
+# print(type(sayi3))
+
+# liste1 = [sayi1,sayi2,sayi3]
+
+# print(type(liste1))
+
+# doğru_mu = True
+# yanlış_mı = False
+# print(type(doğru_mu))
+
+# sayi1 = 42
+# sayi2 = -17
+# ondalik1 = 3.14
+# ondalik2 = -0.5
+# sayi3 = float(sayi1)
+# print("int to float", sayi3)
+
+# ondalik3 = int(ondalik1)
+# print("float to int",ondalik3)
+
+# dogru_mu = True
+# yanlış_mı = False
+# sayi1 = int(dogru_mu)
+# print("bool to int",sayi1)
+# print(type(sayi1))
+
+# kullanıcı_adı = input("lütfen adınızı giriniz:")
+# print("Hoşgeldiniz", kullanıcı_adı)
+ 
+# n1 = int(input("rastgele sayı giriniz"))
+# n2 = int(input("yeni bir sayı giriniz")) 
+
+# print(n1+n2)
+
+# n1 = input("bir sayı giriniz")
+# n2 = input("bir başka sayı giriniz")
+# print(int(n1)+int(n2))
