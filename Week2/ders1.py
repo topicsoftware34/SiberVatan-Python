26e7348fdb3299a92b3304cd27d3f123c05f12ae
topicsoftware34 <155827627+topicsoftware34@users.noreply.github.com>
2024-01-19 19:26:15 +0300

    Update ders1.py

--- Week2/ders1.py
+++ Week2/ders1.py
@@ -1,33 +1,33 @@
-#adi,yaş,sehir=("tarık","16","istanbul")
-#print(adi)
-#print(yaş)
-#print(sehir)
+adi,yaş,sehir=("tarık","16","istanbul")
+print(adi)
+print(yaş)
+print(sehir)
 
-#sayi1=46
-#sayi2=72
-#sayi3=str(sayi1)
-#sayi4=str(sayi2)
-#print(sayi3+sayi4)
+sayi1=46
+sayi2=72
+sayi3=str(sayi1)
+sayi4=str(sayi2)
+print(sayi3+sayi4)
 
 
-#x=0.2
-#y=17002
-#maas=(y-y*x)
-#print(maas)
+x=0.2
+y=17002
+maas=(y-y*x)
+print(maas)
 
-#sayi5=7
-#print("mutlak değer :",abs(sayi5))
+sayi5=7
+print("mutlak değer :",abs(sayi5))
 
-#üs=pow(2,3)
-#print(üs)
+üs=pow(2,3)
+print(üs)
 
-#from math import sqrt,ceil,floor
-#sayi1=25
-#karekok=sqrt(sayi1)
-#print("sayının karekökü:",karekok)
+from math import sqrt,ceil,floor
+sayi1=25
+karekok=sqrt(sayi1)
+print("sayının karekökü:",karekok)
 
-#ondalık=10.35
-#yukarı=ceil(ondalık)
-#aşağı=floor(ondalık)
-#print("yukarı",yukarı)
-#print("aşağı",aşağı)
+ondalık=10.35
+yukarı=ceil(ondalık)
+aşağı=floor(ondalık)
+print("yukarı",yukarı)
+print("aşağı",aşağı)
